ada0bc0fef238be8a2ed925e07ad1a67122e9e02
Derek Dorr <dhdorr@gmail.com>
2025-04-04 22:22:30 -0700

    testing weights

--- ld57-deep-pockets/Scripts/pickable.gd
+++ ld57-deep-pockets/Scripts/pickable.gd
@@ -10,24 +10,28 @@ func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) 
 	if event.is_action_pressed("grab"):
 		print("shape grabbed")
 		StateGrabbed = true
-		self.freeze = true
+		self.gravity_scale = 0
+		#self.freeze = true
 	
 
 func _physics_process(delta: float) -> void:
 	if StateGrabbed:
-		var test = self.position.distance_to(get_viewport().get_mouse_position())
 		
-		if self.position.distance_to(get_viewport().get_mouse_position()) > 1.5:
-			self.move_and_collide(self.position.direction_to(get_viewport().get_mouse_position()) * 2)
-		else:
-			var collision = self.move_and_collide(self.position.direction_to(get_viewport().get_mouse_position()) * 2, true)
-			if not collision:
-				print(test)
-				self.position = get_viewport().get_mouse_position()
+		var distance2 = self.position.distance_squared_to(get_viewport().get_mouse_position())
+		var distance1 = self.position.distance_to(get_viewport().get_mouse_position())
+		var direction = self.position.direction_to(get_viewport().get_mouse_position())
+		var collision = self.move_and_collide(direction * distance1, true)
+		
+		#self.move_and_collide(direction * distance1 / 20)
+		self.linear_velocity = direction * distance1
 	
 
 func _input(event: InputEvent) -> void:
 	if StateGrabbed and event.is_action_released("grab"):
 		print("shape dropped")
 		StateGrabbed = false
-		self.freeze = false
+		self.gravity_scale = 1
+		var distance2 = self.position.distance_squared_to(get_viewport().get_mouse_position())
+		var distance1 = self.position.distance_to(get_viewport().get_mouse_position())
+		var direction = self.position.direction_to(get_viewport().get_mouse_position())
+		self.apply_central_impulse(direction * distance2)
